0c32b67dfe3eed8d029ac6d4b7d3b47d72c41ae4
Ken Horlador <kenhorlador@gmail.com>
2021-07-25 10:07:16 +0800

    add loader and bg loader

--- js/script.js
+++ js/script.js
@@ -6,10 +6,26 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+const bg_loader = document.getElementById('bg-loading')
+
+// Show loading
+function loading() {
+    loader.hidden = false
+    bg_loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+// Hide loading
+function complete() {
+    loader.hidden = true
+    bg_loader.hidden = true
+    quoteContainer.hidden = false
+}
 
 // Show new quote
 function newQuote() {
-    
+    loading()
     // Pick random number
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     
@@ -27,12 +43,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote')
     }
-
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text
+    complete()
 }
 
 // Get quotes from API
 async function getQuotes() {
+    loading()
     const apiUrl = 'https://type.fit/api/quotes'
     try {
         const response = await fetch(apiUrl)
